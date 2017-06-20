8a97986c8bd05c8786b9d89f28f917bd3544d5a4
foxmessenger <iamliamfox@gmail.com>
2017-06-20 15:59:31 -0500

    axios call working and returning information

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -10,56 +10,80 @@ import axios from 'axios';
 // ----------------------------
 class Search extends Component {
 
+    
     constructor(props) {
-        
         super(props);
         this.state = {
             topic: '',
             begin_date: '',
             end_date: ''
         };
-        
 
-        this.handleTopic    = this.handleTopic.bind(this);
-        this.handleSDate    = this.handleSYear.bind(this);
-        this.handleEDate    = this.handleEYear.bind(this);
-        this.handleSubmit   = this.handleSubmit.bind(this);
+        
+        this.handleTopic        = this.handleTopic.bind(this);
+        this.handleStartDate    = this.handleStartDate.bind(this);
+        this.handleEndDate      = this.handleEndDate.bind(this);
+        this.handleSubmit       = this.handleSubmit.bind(this);
     }
+
+    // runQuery() {
+        // const nytAPI = 'a306a58c65134177bed7d0955ace8afa';
+
+        // axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+        //     params: {
+        //         'api-key': nytAPI,
+        //         'q': this.state.topic,
+        //         'begin_date': this.state.begin_date || 20100101,
+        //         'end_date': this.state.end_date || 20170624
+        //     }
+            
+        // }).then((res) => {
+        //     console.log(res);
+        // }).catch((error) => {
+        //     console.log(error);
+        // });
+    // }
 
     handleTopic(event) {
         this.setState({topic: event.target.value});
     }
 
-    handleSYear(event) {
-        this.setState({sYear: event.target.value});
+    handleStartDate(event) {
+        this.setState({begin_date: event.target.value});
     }
 
-    handleEYear(event) {
-        this.setState({eYear: event.target.value});
+    handleEndDate(event) {
+        this.setState({end_date: event.target.value});
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        console.log(this.state)
-        // helpers(event);
-        runQuery: (url) => {
-        // console.log(url)
+        // console.log(this.state)
+        // runQuery();
+
+        const nytAPI = 'a306a58c65134177bed7d0955ace8afa';
 
         axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
             params: {
                 'api-key': nytAPI,
-                q: topic,
-                'begin_date': begin_date || 20100101,
-                'end_date': eDate || 20170624
-
+                'q': this.state.topic,
+                'begin_date': this.state.begin_date || 20100101,
+                'end_date': this.state.end_date || 20170624
             }
             
-        }).then(function (res) {
-            console.log(`this is the response: ${res}`);
-        }).catch(function (error) {
+        }).then((res) => {
+            console.log(res.data.response.docs);
+            this.setState({
+                
+                // clear the query after the call
+                topic: "",
+                begin_date: '',
+                end_date: ''
+
+            })
+        }).catch((error) => {
             console.log(error);
         });
-    }
     }
 
     render() {
@@ -73,17 +97,20 @@ class Search extends Component {
                             <br/>
                             <br/>
                             <p>Start Date</p>
-                            <input className='input--border' type='number' value={this.state.sDate} onChange={this.handleSDate} />
+                            <input className='input--border' type='number' value={this.state.begin_date} onChange={this.handleStartDate} />
                             <br/>
                             <br/>
                             <p>End Date</p>
-                            <input className='input--border' type='number' value={this.state.eDate} onChange={this.handleEDate} />                    
+                            <input className='input--border' type='number' value={this.state.end_date} onChange={this.handleEndDate} />                    
                         </label>
                         <br />
                         <input className='btn btn-success' type='submit' value='Submit' />
                     </form>
-
-                    <div id='results'>
+                    <div className='container'>
+                        <div id='results'>
+                            <div className='articles'>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -12,26 +12,22 @@ const nytAPI = "a306a58c65134177bed7d0955ace8afa";
 // helpers
 // ----------------------------
 
-const helpers = {
-	runQuery: (url) => {
-		console.log(url)
+runQuery() {
+	axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
+	    params: {
+	   		'api-key': nytAPI,
+	   		'q': this.props.topic,
+	   		'begin_date': this.props.begin_date || 20100101,
+	   		'end_date': this.props.end_date || 20170624
 
-		axios.get('https://api.nytimes.com/svc/search/v2/articlesearch.json', {
-		    params: {
-		   		'api-key': nytAPI,
-		   		q: topic,
-		   		'begin_date': sDate || 20100101,
-		   		'end_date': eDate || 20170624
-
-			}
-			
-		}).then(function (res) {
-			console.log(res);
-		}).catch(function (error) {
-			console.log(error);
-		});
-	}
-};
+		}
+		
+	}).then(function (res) {
+		console.log(res);
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
 
 
 export default helpers;
