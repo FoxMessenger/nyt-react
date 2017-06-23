1c98477788f5e4b3392b3dafd25534561cf54c9f
foxmessenger <iamliamfox@gmail.com>
2017-06-23 13:56:00 -0500

    added routes

--- app/app.js
+++ app/app.js
@@ -2,10 +2,19 @@
 // import dependencies
 // ----------------------------
 import React, {Component} from 'react';
-import routes from './config/routes';
+// import routes from './config/routes';
+import routes from '../controllers/routes'
 import ReactDOM from 'react-dom';
+
+
+import Main from "./components/Main";
+import Search from "./components/children/Search";
+import Results from "./components/children/Results";
+
 
 // ----------------------------
 // render routes
 // ----------------------------
+
+
 ReactDOM.render(routes, document.getElementById('app'));
