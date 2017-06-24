6b73549687041c1d46a143cca39288d75d24ce6d
foxmessenger <iamliamfox@gmail.com>
2017-06-24 00:16:52 -0500

    articles visible, articles saving, but saved articles not visible except in JSON form

--- app/components/children/Saved.js
+++ app/components/children/Saved.js
@@ -12,11 +12,12 @@ class Saved extends Component {
     constructor(props) {
         super(props);
         this.state = {articles: null};
+
     }
 
     // getInitialState() {
     //     return {
-    //         articles: res.data
+    //         articles: res.articles
     //     }
     // }
 
@@ -34,44 +35,65 @@ class Saved extends Component {
             })
     }
 
-    componentWillReceiveProps(props) {
-        console.log('this is props.articles: ' + JSON.stringify(props))
-        const myResults = props.articles.map((search, i) => {
-        const click = this.clickToDelete.bind(this, search);
-        return <div className="list-group-item" key={i}><a href={search.url} target="_blank">{search.title}</a>{search.date}<button type="button" className="btn btn-danger" onClick={click}>Delete</button></div>
-    });
+    // componentWillReceiveProps(props) {
+    //     console.log('this is props.articles: ' + JSON.stringify(props))
+    //     const myResults = props.articles.map((search, i) => {
+    //     const click = this.clickToDelete.bind(this, search);
+    //     return <div className='list-group-item' key={i}><a href={search.url} target='_blank'>{search.title}</a>{search.date}<button type='button' className='btn btn-danger' onClick={click}>Delete</button></div>
+    // });
 
-        this.setState({articles: myResults});
-    }
+    //     this.setState({articles: myResults});
+    // }
     
-    componentDidUpdate(prevProps, prevState){
-        console.log("this is the current state: " + JSON.stringify(this.state));
-        if(prevState.topic != this.state.topic){
+    // componentDidUpdate(prevProps, prevState){
+    //     console.log('this is the current state: ' + JSON.stringify(this.state));
+    //     if(prevState.topic != this.state.topic){
             
-            Search.handleSubmit(this.state.topic, this.state.begin_date, this.state.end_date)
-            .then((data) => {
-                console.log(data);
-                if (data != this.state.results)
-                {
-                    this.setState({
-                        results: data
-                    })
-                }
-            })
-        }
-    }
+    //         Search.handleSubmit(this.state.topic, this.state.begin_date, this.state.end_date)
+    //         .then((data) => {
+    //             console.log(data);
+    //             if (data != this.state.results)
+    //             {
+    //                 this.setState({
+    //                     results: data
+    //                 })
+    //             }
+    //         })
+    //     }
+    // }
 
+    savedArticles() {
+        return this.state.results.data.map((article) => {
+            return (
+                <div className = 'panel panel-primary' key={article._id}>
+                    <div className = 'panel-heading'>
+                        <h3 className = 'panel-title'>{article.title}</h3>
+                    </div>
+                    <div className = 'panel-body'>
+                        <p><a href = {article.url}>Read about it</a></p>
+                        <p>Written on: {new Date(article.date).toString()}</p>
+                        <button
+                            className = 'btn btn-danger'
+                            value = {article._id}
+                            onClick = {this.deleteArticle}>
+                            delete
+                        </button>
+                    </div>
+                </div>
+            )
+        })
+    }
 
     render() {
 
         return(
 
-            <div className="panel panel-success">
-                <div className="panel-heading">
-                    <h3 className="panel-title text-center">Saved Articles</h3>
+            <div className='panel panel-success'>
+                <div className='panel-heading'>
+                    <h3 className='panel-title text-center'>Saved Articles</h3>
                 </div>
-                <div className="panel-body">
-                    {this.state.articles}
+                <div className='panel-body'>
+                    {this.props.results ? this.savedArticles() : <div></div>}
                 </div>
             </div>
 
